8ecec0320134bb6674f9443ab8d8e4b6e0937791
mikitahap <cyclingnerd4@hotmail.com>
2025-03-28 17:04:09 +0100

    fix(history): correct saving logic for non authorised users

--- translate.js
+++ translate.js
@@ -59,7 +59,7 @@ function debounce(func, delay) {
 }
 function shouldSaveHistory() {
     const username = sessionStorage.getItem('username');
-    if (!username) return false;
+    if (!username) return true``;
 
     const settings = JSON.parse(localStorage.getItem(`userSettings_${username}`)) || {};
     return settings.saveHistory !== false;
